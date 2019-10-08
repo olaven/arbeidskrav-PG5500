d579b60150093207f38fa49549830bcc17759074
olaven <olav@sundfoer.com>
2019-10-08 15:52:43 +0200

    first pass at documentation

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Arbeidskrav - Embedded Systems 
 
+## Overordnet 
+I denne oppgaven har jeg koplet sammen en Arduino UNO med et 8x8 LED-display
+()
+ved hjelp av en MAX7219. Jeg har deretter skrevet et program som skriver ut 
+scrollende tekst, slik som oppgaven krever. Programmet er skrevet med
+LedControl-biblioteket. 
+
+## Kobling 
+Jeg har brukt dataark for [MAX7219](https://datasheets.maximintegrated.com/en/ds/MAX7219-MAX7221.pdf) og [matrisen](http://megtestesules.info/hobbielektronika/adatlapok/LED8x8_1088AS.pdf). 
+Dataarkene brukte jeg til aa lage min [egen oversikt](#skjema) over hvordan ting skulle kobles. 
+
+Matrisen sitt dataark viser hvilken PIN paa mtrisen som styrer hvilken kolonne/rad. Videre fant jeg ut at hver 
+dig0-7 paa MAX7219 gikk til rader og SIG DP og SIG A-G gikk til kolonner. Basert paa denne informasjonen kunne 
+jeg lage en oversikt over hvilke PINs paa MAX7219 som skulle til forskjellige PINs paa matrisen. 
+
+## Virkemaate 
+Matrisen fungerer i utgangspunktet slik: det dannes en krets ved aa sette stroem gjennom en gitt kolonne og en gitt rad, 
+med tilfoersel i den ene enden og jording i den andre. Da lyser man opp en LED-paere. 
+
+Dette er ikke nok; noen kombinasjoner er uforenelige med denne fremgangsmaaten. Dette kommer man rundt 
+ved at matrisen blinker fort. Det gaar saa fort at vaare mennesklige oeyene ikke oppdager det. I tillegg 
+sparer det stroem :) 
+
+
+MAX7219 lar oss bruke matrisen uten aa koble den rett i Arduino-en. Vi kommer unna med aa bruke 3 PINs paa 
+Arduino-brettet (eks. 5V og GND) istedenfor 16. Da har vi rom til aa gjoere andre flere ting med Arduino-brettet samtidig. 
+
+
+# Skjema
+![bilde av mitt skjema](./skjema.jpg)
--- src/main.ino
+++ src/main.ino
@@ -13,10 +13,8 @@ void setup()
 
 void write_letter(byte letter[5], int adjustment)
 {
-
     for(int i = 0; i < 5; i++)
     {
-        
         int adjusted_row = i + adjustment;
         lc.setRow(0, adjusted_row, letter[i]);
     }
@@ -24,10 +22,8 @@ void write_letter(byte letter[5], int adjustment)
 
 void scroll_word(byte * word[7]) 
 {
-
     for (int i = 0; i < 7; i++)
     {
-
         int adjustment = (i * 5) + beginning_position;
         write_letter(word[i], adjustment);
     }
@@ -36,30 +32,34 @@ void scroll_word(byte * word[7])
 
 void write_arduino_on_matrix()
 {
-
-    byte a[5] = {B01111110, B00010001, B00010001, B00010001, B01111110}; //no need
-    byte r[5] = {B01111100, B00001000, B00001000, B00000100, B00001000}; //flipped
-    byte d[5] = {B00111000, B01000100, B01000100, B01001000, B01111111}; //flipped 
-    byte u[5] = {B00111100, B01000000, B01000000, B00100000, B01111100}; //flipped
-    byte i[5] = {B00000000, B01000100, B01111101, B01000000, B00000000}; //flipped 
-    byte n[5] = {B01111100, B00001000, B00000100, B00000100, B01111000}; //flipped
-    byte o[5] = {B00011100, B00100010, B00100010, B00100010, B00011100}; //no need
+    byte empty = B00000000; 
+    byte a[5] = {B01111110, B00010001, B00010001, B00010001, B01111110}; 
+    byte r[5] = {B01111100, B00001000, B00001000, B00000100, B00001000}; 
+    byte d[5] = {B00111000, B01000100, B01000100, B01001000, B01111111}; 
+    byte u[5] = {B00111100, B01000000, B01000000, B00100000, B01111100}; 
+    byte i[5] = {B00000000, B01000100, B01111101, B01000000, B00000000}; 
+    byte n[5] = {B01111100, B00001000, B00000100, B00000100, B01111000}; 
+    byte o[5] = {B00011100, B00100010, B00100010, B00100010, B00011100}; 
 
     byte * arduino[7] = {a, r, d, u, i, n, o};
     scroll_word(arduino);
 }
 
-//NOTE: not used yet 
-int adjust_beginning(int current) 
+int adjusted_beginning(int current) 
 {
-
-    return current--; 
+    int restart_point = -7 * 5;
+    if (current == restart_point)
+    {
+        return 8; 
+    }
+    
+    return --current; 
 }
 
 void loop()
 {
     lc.clearDisplay(0); 
     write_arduino_on_matrix();
-    beginning_position--; //TODO: adjust back when entire word is shown
+    beginning_position = adjusted_beginning(beginning_position);
     delay(delaytime);
 }
