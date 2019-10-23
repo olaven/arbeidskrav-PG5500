b1e0a21779c5a4a578134c546ebd9ecc2ebe2c8c
olaven <olav@sundfoer.com>
2019-10-23 22:29:17 +0200

    alarm is triggered on movement

--- alarm/src/main.ino
+++ alarm/src/main.ino
@@ -15,27 +15,25 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 // Clock definitions:
 #include "RTClib.h"
 RTC_DS1307 rtc;
-
 // Speaker definitions:
 #include "Tone.h"
 const int speaker_pin = 7;
 Tone player;
-
 // Button definitions
 const int buttons[] = {1, 2, 3, 4};
-
 // Distance sensor definitions
-const int trigger_pin = 5; //Trig pin
-const int echo_pin = 6;    //Echo pin
+const int trigger_pin = 5; 
+const int echo_pin = 6;    
+int previous_distance = 0; 
 
-const bool alarm_triggered = false;
+// general logic 
+int run_counter = 0; 
+bool alarm_triggered = false;
 const char *last_trigger_timestamp = "never triggered";
 
 void init_display()
 {
-
     tft.initR(INITR_BLACKTAB);
-    tft.fillScreen(ST77XX_BLACK);
     tft.invertDisplay(true);
 }
 
@@ -57,14 +55,12 @@ void init_speaker()
 
 void init_distance_sensor()
 {
-
     pinMode(trigger_pin, OUTPUT); // Trigger is an output pin
     pinMode(echo_pin, INPUT);
 }
 
 void setup(void)
 {
-
     Serial.begin(9600);
     init_display();
     init_clock();
@@ -74,12 +70,9 @@ void setup(void)
 
 void draw_text(char *text, uint16_t color)
 {
-    Serial.println(text);
-    Serial.println(color);
     tft.setCursor(0, 0);
     tft.setTextColor(color);
     tft.setTextWrap(true);
-    tft.setTextSize(20);
     tft.print(text);
 }
 
@@ -112,6 +105,7 @@ long get_current_distance()
 void render_idle_screen()
 {
 
+    //TODO: render current time 
     tft.fillScreen(ST7735_GREEN);
 
     char *timestamp = get_current_timestamp();
@@ -120,16 +114,30 @@ void render_idle_screen()
 
 void render_triggered_screen()
 {
-
-    //TODO: Implement
+    //TODO: render triggered time 
+    tft.fillScreen(ST7735_BLUE); //renders as RED. Error in library?
+    player.play(NOTE_C4);
 }
 
-//NOTE: avstandssensor på 6 og 5
 void loop()
 {
-
+    
     long distance = get_current_distance();
-    Serial.println(distance); 
+    run_counter += 1; 
+
+    if (run_counter % 10 == 0) {
+
+        if (run_counter == 0) return; //will have invalid `previous_distance` 
+        long difference = distance - previous_distance; 
+        if (difference > 250 || difference < -250) {
+
+            last_trigger_timestamp = get_current_timestamp(); 
+            alarm_triggered = true; 
+        }
+    }
+
+    previous_distance = distance; 
+
 
     if (alarm_triggered)
     {
